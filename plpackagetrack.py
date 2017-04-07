7ecc93a6e145f4d47a2c37ccc67abe7e87796857
Krzysztof Koziarek <k.koziarek@bankier.pl>
2017-04-07 17:04:57 +0200

    Sanitization

---
+++ plpackagetrack.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+
+from providers import *
+
+class tracking(object):
+    def __init__(self):
+        self._providers = {}
+        
+        for module in sys.modules.keys():
+            if module.startswith('providers.'):
+                self._providers[module[10:]] = globals()[module[10:]]
+
+    def providers(self):
+        return(list(self._providers.keys()))
+        
+    def track(self,provider,number):
+        return(getattr(self._providers[provider], 'track')(number))
+        
+
+
+    
